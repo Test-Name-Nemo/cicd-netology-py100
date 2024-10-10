b65b7227b987a868e896590a4edfd1e50e00ffa1
ortariot <ortariot@gmail.com>
2024-10-11 00:27:13 +0700

    wf0

--- webpython/demo/tests.py
+++ webpython/demo/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class MyTest(TestCase):
+    
+    def test_one(self):
+        self.assertTrue(True)
+    
+    def test_two(self):
+        self.assertFalse(False)
+
